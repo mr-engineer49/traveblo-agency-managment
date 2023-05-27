15cd1145e23036c535bf855c8fe6065a209fbc10
unknown <enea>
2023-05-27 19:34:24 +0200

    changes

--- webdemo/settings.py
+++ webdemo/settings.py
@@ -136,9 +136,6 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
